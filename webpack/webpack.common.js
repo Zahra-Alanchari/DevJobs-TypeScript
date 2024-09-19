b29faa5b6beac5a7cae78315731b6ced2da8c7db
Zahra Alanchari <zahraalanchari@Zahras-MacBook-Pro.local>
2024-09-20 00:55:04 +0330

    fix webpack config for vercel

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -46,6 +47,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '..', './src/index.html'),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'public', to: 'public' }],
     }),
   ],
   stats: 'errors-only',
